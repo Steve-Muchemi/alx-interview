ef87f8675de2662a02416f8217d4200d5cb2cb1c
stewie-gil <gilliganstewie7@gmail.com>
2023-08-17 15:36:52 -0700

    fixed pycodestle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""This file contains the solution to log parsing question"""
+
+
 import sys
 import datetime
+
 
 total_file_size = 0
 status_code_counts = {}
@@ -17,7 +21,8 @@ try:
             file_size = int(parts[8])
 
             total_file_size += file_size
-            status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
+            status_code_counts[status_code] = status_code_counts.get(
+                status_code, 0) + 1
 
             if line_number % 10 == 0:
                 print("File size: {}".format(total_file_size))
@@ -25,7 +30,7 @@ try:
                     print("{}: {}".format(code, status_code_counts[code]))
         except (ValueError, IndexError):
             continue
-except  KeyboardInterrupt:
+except KeyboardInterrupt:
     pass
 
 finally:
