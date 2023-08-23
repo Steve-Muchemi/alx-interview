88bd59e1f4c52b70c9e604b4d2a410d3fe0ad5af
stewie-gil <gilliganstewie7@gmail.com>
2023-08-23 09:41:20 -0700

    fixed pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,25 +15,25 @@ def validUTF8(data: List[int]) -> bool:
     points = 0
 
     for byte in data:
-        
+
         if points == 0:
             if byte >> 3 == 0b11110:
                 points = 3
-                
+
             elif byte >> 4 == 0b1110:
                 points = 2
-                
+
             elif byte >> 5 == 0b110:
                 points = 1
-                
+
             elif byte >> 7 == 0:
                 points = 0
-                
+
             else:
-               
+
                 return False
         else:
-            
+
             if byte >> 6 != 0b10:
                 return False
             points = points - 1
