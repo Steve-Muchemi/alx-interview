837702168cd6ceb1d8dd953770a89869d8daa925
stewie-gil <gilliganstewie7@gmail.com>
2023-10-06 01:34:27 +0000

    m

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def is_prime(num):
     if num < 2:
         return False
@@ -8,14 +9,15 @@ def is_prime(num):
             return False
     return True
 
+
 def isWinner(x, nums):
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         primes = [i for i in range(2, n + 1) if is_prime(i)]
         available_numbers = set(range(1, n + 1))
-        
+
         maria_turn = True
         while True:
             valid_moves = [p for p in primes if p in available_numbers]
@@ -26,18 +28,17 @@ def isWinner(x, nums):
                 else:
                     maria_wins += 1
                 break
-            
+
             # Make a move.
             move = max(valid_moves) if maria_turn else min(valid_moves)
             available_numbers -= set(range(move, n + 1, move))
-            
+
             # Switch to the other player's turn.
             maria_turn = not maria_turn
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif maria_wins < ben_wins:
         return "Ben"
     else:
         return None
-
